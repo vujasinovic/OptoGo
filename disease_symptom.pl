7059dd9da2835009b5b34b94cf66b8bb29bdaf64
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-04-14 13:14:24 +0200

    Diseases with symtoms

---
+++ disease_symptom.pl
@@ -0,0 +1,111 @@
+% astigmatism
+disease_symptom(astigmatism, diminished_vision, 70.0).
+disease_symptom(astigmatism, symptoms_of_eye, 29.0).
+disease_symptom(astigmatism, lacrimation, 24.0).
+disease_symptom(astigmatism, spots_of_clouds_in_vision, 20.0).
+disease_symptom(astigmatism, eye_redness, 16.0).
+disease_symptom(astigmatism, itchiness_of_eye, 16.0).
+disease_symptom(astigmatism, pain_in_eye, 16.0).
+disease_symptom(astigmatism, foreign_body_sensation_in_eye, 11.0).
+disease_symptom(astigmatism, eye_deviation, 9.0).
+disease_symptom(astigmatism, eye_moves_abnormally, 9.0).
+disease_symptom(astigmatism, cross_eyed, 6.0).
+disease_symptom(astigmatism, double_vision, 6.0).
+
+% hyperopia
+disease_symptom(hyperopia, diminished_vision, 70.0).
+disease_symptom(hyperopia, symptoms_of_eye, 70.0).
+disease_symptom(hyperopia, abnormal_movement_of_eyelid, 24.0).
+disease_symptom(hyperopia, itchiness_of_eye, 21.0).
+disease_symptom(hyperopia, pain_in_eye, 21.0).
+disease_symptom(hyperopia, eye_deviation, 21.0).
+disease_symptom(hyperopia, spots_of_clouds_in_vision, 17.0).
+disease_symptom(hyperopia, eye_redness, 14.0).
+disease_symptom(hyperopia, eye_moves_abnormally, 5.0).
+disease_symptom(hyperopia, blindness, 5.0).
+disease_symptom(hyperopia, neck_cramps_of_spasms, 5.0).
+disease_symptom(hyperopia, difficulty_speaking, 5.0).
+
+% myopia
+disease_symptom(myopia, diminished_vision, 72.0).
+disease_symptom(myopia, spots_of_clouds_in_vision, 31.0).
+disease_symptom(myopia, pain_in_eye, 27.0).
+disease_symptom(myopia, symptoms_of_eye, 23.0).
+disease_symptom(myopia, eye_redness, 15.0).
+disease_symptom(myopia, foreign_body_sensation_in_eye, 9.0).
+disease_symptom(myopia, itchiness_of_eye, 9.0).
+disease_symptom(myopia, lacrimation, 9.0).
+disease_symptom(myopia, eye_moves_abnormally, 6.0).
+disease_symptom(myopia, blindness, 5.0).
+disease_symptom(myopia, eye_deviation, 3.0).
+disease_symptom(myopia, abnormal_movement_of_eyelid, 3.0).
+
+% blepharitis
+disease_symptom(blepharitis, diminished_vision, 64.0).
+disease_symptom(blepharitis, eye_redness, 56.0).
+disease_symptom(blepharitis, pain_in_eye, 54.0).
+disease_symptom(blepharitis, itchiness_of_eye, 47.0).
+disease_symptom(blepharitis, swollen_eye, 46.0).
+disease_symptom(blepharitis, symptoms_of_eye, 43.0).
+disease_symptom(blepharitis, lacrimation, 40.0).
+disease_symptom(blepharitis, spots_of_clouds_in_vision, 29.0).
+disease_symptom(blepharitis, eyelid_swelling, 26.0).
+disease_symptom(blepharitis, white_discharge_from_eye, 24.0).
+disease_symptom(blepharitis, eye_burns_or_stings, 21.0).
+disease_symptom(blepharitis, eyelid_lesion_or_rash, 21.0).
+
+% stye
+disease_symptom(stye, pain_in_eye, 74.0).
+disease_symptom(stye, swollen_eye, 73.0).
+disease_symptom(stye, eye_redness, 60.0).
+disease_symptom(stye, eyelid_swelling, 56.0).
+disease_symptom(stye, symptoms_of_eye, 46.0).
+disease_symptom(stye, mass_on_eyelid, 35.0).
+disease_symptom(stye, itchiness_of_eye, 27.0).
+disease_symptom(stye, eyelid_lesion_or_rash, 25.0).
+disease_symptom(stye, abnormal_appearing_skin, 19.0).
+disease_symptom(stye, eye_burns_or_stings, 12.0).
+disease_symptom(stye, skin_swelling, 9.0).
+disease_symptom(stye, lacrimation, 9.0).
+
+% ectropion
+disease_symptom(ectropion, lacrimation, 72.0).
+disease_symptom(ectropion, pain_in_eye, 66.0).
+disease_symptom(ectropion, abnormal_movement_of_eyelid, 58.0).
+disease_symptom(ectropion, symptoms_of_eye, 58.0).
+disease_symptom(ectropion, eye_redness, 47.0).
+disease_symptom(ectropion, spots_of_clouds_in_vision, 30.0).
+disease_symptom(ectropion, eyelid_lesion_or_rash, 30.0).
+disease_symptom(ectropion, foreign_body_sensation_in_eye, 30.0).
+disease_symptom(ectropion, swollen_eye, 30.0).
+disease_symptom(ectropion, diminished_vision, 30.0).
+disease_symptom(ectropion, white_discharge_from_eye, 30.0).
+disease_symptom(ectropion, itchiness_of_eye, 30.0).
+
+% trichiasis
+disease_symptom(trichiasis, foreign_body_sensation_in_eye, 80.0).
+disease_symptom(trichiasis, eyelid_lesion_or_rash, 63.0).
+disease_symptom(trichiasis, diminished_vision, 44.0).
+disease_symptom(trichiasis, symptoms_of_eye, 44.0).
+disease_symptom(trichiasis, itchiness_of_eye, 44.0).
+disease_symptom(trichiasis, pain_in_eye, 44.0).
+disease_symptom(trichiasis, eye_redness, 28.0).
+disease_symptom(trichiasis, swollen_eye, 28.0).
+disease_symptom(trichiasis, emotional_symptoms, 2.0).
+disease_symptom(trichiasis, back_weakness, 2.0).
+disease_symptom(trichiasis, elbow_cramps_or_spasms, 2.0).
+disease_symptom(trichiasis, pus_in_sputum, 2.0).
+
+% bell palsy
+disease_symptom(bell_palsy, focal_weakness, 82.0).
+disease_symptom(bell_palsy, loss_of_sensation, 70.0).
+disease_symptom(bell_palsy, symptoms_of_face, 67.0).
+disease_symptom(bell_palsy, headache, 51.0).
+disease_symptom(bell_palsy, facial_pain, 39.0).
+disease_symptom(bell_palsy, weakness, 28.0).
+disease_symptom(bell_palsy, peripheral_edema, 24.0).
+disease_symptom(bell_palsy, symptoms_of_eye, 21.0).
+disease_symptom(bell_palsy, paresthesia, 19.0).
+disease_symptom(bell_palsy, diminished_vision, 19.0).
+disease_symptom(bell_palsy, eyelid_lesion_or_rash, 16.0).
+disease_symptom(bell_palsy, abnormal_involuntary_movements, 16.0).
