7689fc79f0d451fcc8b70c92de2afa124b8044b9
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-04-14 13:49:21 +0200

    Symptom-diseases relations

---
+++ symptom_disease.pl
@@ -0,0 +1,86 @@
+% diminished_vision
+symptom_disease(diminished_vision, cataract, 25.0).
+symptom_disease(diminished_vision, macular_degeneration, 10.0).
+symptom_disease(diminished_vision, chronic_glaucoma, 7.0).
+symptom_disease(diminished_vision, diabetic_retinopathy, 5.0).
+symptom_disease(diminished_vision, myopia, 4.0).
+symptom_disease(diminished_vision, dry_eye_of_unknown_cause, 4.0).
+
+% symptoms_of_eye
+symptom_disease(symptoms_of_eye, dry_eye_of_unknown_cause, 12.0).
+symptom_disease(symptoms_of_eye, cataract, 10.0).
+symptom_disease(symptoms_of_eye, conjunctivitis, 7.0).
+symptom_disease(symptoms_of_eye, chronic_glaucoma, 12.0).
+
+% lacrimation
+symptom_disease(lacrimation, conjunctivitis, 13.0).
+symptom_disease(lacrimation, cataract, 8.0).
+symptom_disease(lacrimation, dry_eye_of_unknown_cause, 7.0).
+symptom_disease(lacrimation, conjunctivitis, 7.0).
+
+% spots_of_clouds_in_vision
+symptom_disease(spots_of_clouds_in_vision, cataract, 23.0).
+symptom_disease(spots_of_clouds_in_vision, vitreous_degeneration, 15.0).
+symptom_disease(spots_of_clouds_in_vision, macular_degeneration, 13.0).
+symptom_disease(spots_of_clouds_in_vision, chronic_glaucoma, 9.0).
+symptom_disease(spots_of_clouds_in_vision, floaters, 8.0).
+symptom_disease(spots_of_clouds_in_vision, retinal_detachment, 5.0).
+symptom_disease(spots_of_clouds_in_vision, myopia, 5.0).
+
+% eye_redness
+symptom_disease(eye_redness, conjunctivitis, 39.0).
+
+% eye_moves_abnormally
+symptom_disease(eye_moves_abnormally, myopia, 8.0).
+symptom_disease(eye_moves_abnormally, eye_alignment_disorder, 8.0).
+symptom_disease(eye_moves_abnormally, astigmatism, 6.0).
+symptom_disease(eye_moves_abnormally, chronic_glaucoma, 2.0).
+
+% blindness
+symptom_disease(blindness, cataract, 12.0).
+symptom_disease(blindness, chronic_glaucoma, 8.0).
+symptom_disease(blindness, macular_degeneration, 6.0).
+symptom_disease(blindness, retinal_detachment, 5.0).
+symptom_disease(blindness, optic_neuritis, 4.0).
+
+% itchness_of_eye
+symptom_disease(itchness_of_eye, conjunctivitis, 20.0).
+symptom_disease(itchness_of_eye, dry_eye_of_unknown_cause, 7.0).
+symptom_disease(itchness_of_eye, cataract, 7.0).
+symptom_disease(itchness_of_eye, blepharitis, 4.0).
+
+% foreign_body_sensation_in_eye
+symptom_disease(foreign_body_sensation_in_eye, cataract, 10.0).
+symptom_disease(foreign_body_sensation_in_eye, dry_eye_of_unknown_cause, 10.0).
+symptom_disease(foreign_body_sensation_in_eye, chronic_glaucoma, 9.0).
+symptom_disease(foreign_body_sensation_in_eye, conjunctivitis, 8.0).
+symptom_disease(foreign_body_sensation_in_eye, foreign_body_in_the_eye, 8.0).
+symptom_disease(foreign_body_sensation_in_eye, corneal_abrasion, 8.0).
+symptom_disease(foreign_body_sensation_in_eye, macular_degeneration, 4.0).
+
+% eye_deviation
+symptom_disease(eye_deviation, eye_alignment_disorder, 49.0).
+symptom_disease(eye_deviation, amblyopia, 19.0).
+symptom_disease(eye_deviation, hyperopia, 12.0).
+symptom_disease(eye_deviation, astigmatism, 7.0).
+symptom_disease(eye_deviation, myopia, 5.0).
+symptom_disease(eye_deviation, macular_degeneration, 2.0).
+symptom_disease(eye_deviation, chronic_glaucoma, 2.0).
+
+% cross_eyed
+symptom_disease(cross_eyed, eye_alignment_disorder, 73.0).
+symptom_disease(cross_eyed, astigmatism, 9.0).
+symptom_disease(cross_eyed, amblyopia, 9.0).
+symptom_disease(cross_eyed, hyperopia, 5.0).
+symptom_disease(cross_eyed, myopia, 5.0).
+
+% double_vision
+symptom_disease(double_vision, cataract, 10.0).
+symptom_disease(double_vision, eye_alignment_disorder, 8.0).
+symptom_disease(double_vision, macular_degeneration, 4.0).
+symptom_disease(double_vision, chronic_glaucoma, 3.0).
+
+% abnormal_movement_of_eyelid
+symptom_disease(abnormal_movement_of_eyelid, cataract, 7.0).
+symptom_disease(abnormal_movement_of_eyelid, blepharospasm, 5.0).
+symptom_disease(abnormal_movement_of_eyelid, hyperopia, 4.0).
