c9235c8c80482cadd222a33d8c8e62b1de9f989f
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-06-01 15:55:29 +0200

    Create repositories for entities

---
+++ src/com/optogo/repository/impl/DiseaseRepository.java
@@ -0,0 +1,57 @@
+package com.optogo.repository.impl;
+
+import com.optogo.model.Disease;
+import com.optogo.repository.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class DiseaseRepository implements Repository<Disease> {
+    public static final String DEFAULT_UNIT = "default-unit";
+
+    private EntityManager em;
+
+    public DiseaseRepository() {
+        em = Persistence.createEntityManagerFactory(DEFAULT_UNIT).createEntityManager();
+    }
+
+    @Override
+    public Disease save(Disease disease) {
+        em.getTransaction().begin();
+        Disease savedDisease = em.merge(disease);
+        em.getTransaction().commit();
+        return savedDisease;
+    }
+
+    @Override
+    public Disease update(Disease disease) {
+        em.getTransaction().begin();
+        Disease updateDisease = em.merge(disease);
+        em.getTransaction().commit();
+        return updateDisease;
+    }
+
+    @Override
+    public List<Disease> findAll() {
+        return (List<Disease>) em.createQuery("SELECT d FROM Disease d").getResultList();
+    }
+
+    @Override
+    public Disease findById(Long id) {
+        return (Disease) em.createQuery("SELECT d FROM Disease d WHERE d.id = :id").setParameter("id", id).getSingleResult();
+    }
+
+    @Override
+    public void delete(Disease disease) {
+        em.getTransaction().begin();
+        em.remove(disease);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        Disease d = (Disease) em.createQuery("SELECT d FROM Disease d WHERE d.id = :id").setParameter("id", id).getSingleResult();
+        return d != null;
+    }
+}
