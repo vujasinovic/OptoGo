cc8c980ba7fd582f1eb45c81ef06a9154b7b15ce
Pavle Jankovic <pavle.gp@gmail.com>
2019-06-02 12:33:16 +0200

    Logo placeholder

--- src/com/optogo/controller/MainController.java
+++ src/com/optogo/controller/MainController.java
@@ -7,9 +7,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Control;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class MainController {
+    public TextFlow txtFlow;
+
     @FXML
     private BorderPane patientList;
 
@@ -17,6 +24,16 @@ public class MainController {
     private PatientListController patientListController;
 
     public void initialize() {
+        Font font = Font.font("Verdana", FontWeight.BOLD, 25);
+        Text opto = new Text("Opto");
+        opto.setFill(Color.CADETBLUE);
+        opto.setFont(font);
+
+        Text go = new Text("Go");
+        go.setFont(font);
+        go.setFill(Color.INDIANRED);
+
+        txtFlow.getChildren().addAll(opto, go);
     }
 
     public void start(ActionEvent actionEvent) {
