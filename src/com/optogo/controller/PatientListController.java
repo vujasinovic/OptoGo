e988516d1bf570a699d7d7a404a373a06519a908
Pavle Jankovic <pavle.gp@gmail.com>
2019-06-02 11:59:34 +0200

    PatientListController with repo

--- src/com/optogo/controller/PatientListController.java
+++ src/com/optogo/controller/PatientListController.java
@@ -1,10 +1,10 @@
 package com.optogo.controller;
 
 import com.optogo.model.Patient;
-import com.optogo.utils.enums.GenderType;
-import com.optogo.utils.enums.MaritalStatus;
+import com.optogo.repository.impl.PatientRepository;
 import com.optogo.view.dialog.PatientEditorDialog;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Control;
 import javafx.scene.control.TableColumn;
@@ -13,28 +13,24 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 
-public class PatientListController {
+public class PatientListController implements ListChangeListener<Patient> {
     final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     public TableView<Patient> tblPatients;
+    private PatientRepository repository;
 
-    final ObservableList<Patient> patients = FXCollections.observableArrayList(
-            new Patient("Imenko", "Prezimenko", GenderType.MALE, "Danila Kisa 44", LocalDate.of(1996, 12, 22), "00000000", "bb", MaritalStatus.NEVER_MARRIED),
-            new Patient("Imenko1", "Prezimenko1", GenderType.FEMALE, "Danila Kisa 44a", LocalDate.of(1952, 1, 9), "012/111-545", "cc", MaritalStatus.DIVORCED),
-            new Patient("Imenko2", "Prezimenko2", GenderType.MALE, "Danila Kisa 44b", LocalDate.of(1955, 3, 1), "025/122-222", "d", MaritalStatus.SEPARATED),
-            new Patient("Imenko3", "Prezimenko3", GenderType.FEMALE, "Danila Kisa 44c", LocalDate.of(1990, 12, 15), "021/125-555", "e", MaritalStatus.MARRIED),
-            new Patient("Gender", "Bender", GenderType.OTHER, "Danila Kisa 44 prva soba desno", LocalDate.of(1992, 1, 15), "020/545-545", "ii", MaritalStatus.WIDOWED)
-    );
+    final ObservableList<Patient> patients;
 
     private TableColumn firstNameCol;
     private TableColumn lastNameCol;
     private TableColumn dateOfBirthCol;
 
     public PatientListController() {
+        repository = new PatientRepository();
+        patients = FXCollections.observableArrayList(repository.findAll());
+        patients.addListener(this);
     }
 
     public void initialize() {
@@ -66,7 +62,37 @@ public class PatientListController {
         }
     }
 
+    public ObservableList<Patient> getModel() {
+        return patients;
+    }
+
+    public Patient getSelected() {
+        int i = tblPatients.getSelectionModel().getSelectedIndex();
+        if (i >= 0 && i < patients.size()) {
+            return patients.get(i);
+        } else {
+            return null;
+        }
+    }
+
+    public int getSelectedIndex() {
+        return tblPatients.getSelectionModel().getSelectedIndex();
+    }
+
     public ObservableList<Patient> getPatients() {
         return patients;
     }
+
+    @Override
+    public void onChanged(Change change) {
+        change.next();
+        if (change.wasReplaced()) {
+            repository.update(patients.get(change.getFrom()));
+        } else if (change.wasAdded()) {
+            repository.save(patients.get(change.getFrom()));
+        } else if (change.wasRemoved()) {
+            repository.delete((Patient) change.getRemoved().get(0));
+        }
+    }
+
 }
