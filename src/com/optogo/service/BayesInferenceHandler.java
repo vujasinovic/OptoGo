3c5dfe4fc0e978c66af83adff120e4bd17179fdd
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-06-03 19:14:27 +0200

    Add javadoc

--- src/com/optogo/service/BayesInferenceHandler.java
+++ src/com/optogo/service/BayesInferenceHandler.java
@@ -18,8 +18,13 @@ import java.util.Set;
 import static com.optogo.service.BayesInferenceHandlerUtilities.print;
 import static com.optogo.service.BayesInferenceHandlerUtilities.saveFile;
 
+/**
+ * @author avujasinovic
+ * This class is used to form the Bayesian network. It contains methods that are used
+ * to create network and it's nodes and edges between them. It also contains two helper
+ * methods (setFindings and initializeNodes)
+ */
 public class BayesInferenceHandler {
-
     public static final String DISEASE_SYMPTOM_FILEPATH = "resources/disease_symptom.txt";
     public static final float SYMPTOM_INIT_PROBABILITY = 0.5f;
     public static final String YES_STATE = "y";
@@ -29,6 +34,22 @@ public class BayesInferenceHandler {
     public static final String STATE_POSITIVE = "positive";
     public static final String STATE_NEGATIVE = "negative";
 
+    /**
+     * Iterates through all diseases and for each of them it creates independent Bayesian network with the name
+     * of disease. Inside of each network it creates <code>diseaseNode</code> and initialize it's name and states.
+     * After the node is added to network, it creates <code>PotentialTable</code> instance for that node.
+     * Next, it creates nodes for all provided symptoms using the method <code>createSymptomNodes</code>.
+     * After symptom nodes are created and added to network, it calculates all conditional probabilities and add them
+     * to CPT (conditional probability table). Then it runs the algorithm, sets findings and updates the network with
+     * new evidences. In the end it saves the file for each disease with <bold>.net</bold> extension which can be
+     * opened in UnbBayes software.
+     *
+     * @param symptoms - Represents the symptoms that are collected directly from patient.
+     * @return - sorted Map<String, Float> where key represents the name of Disease and value represents the calculated
+     * probability based on provided symptoms
+     * @throws FileNotFoundException  - if file that contains data relations between disease and symptom doesn't exists
+     * @throws InvalidParentException - if it is not possible to create edge between two nodes
+     */
     public Map<String, Float> createNodes(List<String> symptoms) throws FileNotFoundException, InvalidParentException {
         List<String> diseases = DiseaseSymptomParser.getDiseases(DISEASE_SYMPTOM_FILEPATH);
         Map<String, Float> diseaseProbability = new HashMap<>();
@@ -70,6 +91,14 @@ public class BayesInferenceHandler {
         return bayesInferenceHandlerUtilities.sortByValueAscending(diseaseProbability);
     }
 
+    /**
+     * Method that creates nodes for each symptom provided and edges between each symptom and disease.
+     *
+     * @param network     - Bayesian network in which nodes are inserted.
+     * @param diseaseNode - represents the disease with which symptom node will be connected.
+     * @param symptoms    - symptoms that are going to be inserted as nodes in Bayesian network.
+     * @throws InvalidParentException - if it is not possible to create Edge between symptom and disease
+     */
     public void createSymptomNodes(ProbabilisticNetwork network, ProbabilisticNode diseaseNode, Set<String> symptoms) throws InvalidParentException {
         for (String s : symptoms) {
             ProbabilisticNode symptom = new ProbabilisticNode();
@@ -87,6 +116,14 @@ public class BayesInferenceHandler {
         }
     }
 
+    /**
+     * It sets state values of provided symptoms to 1.0 or 0.0 whether provided symptom is in the list of
+     * all symptoms that are specific for disease.
+     *
+     * @param network          - Bayesian network for current disease.
+     * @param providedSymptoms - symptoms collected directly from patient.
+     * @param diseaseSymptoms  - all symptoms that are connected with certain disease.
+     */
     private void setFindings(ProbabilisticNetwork network, List<String> providedSymptoms, Set<String> diseaseSymptoms) {
         for (String s : diseaseSymptoms) {
             ProbabilisticNode factNode = (ProbabilisticNode) network.getNode(s);
@@ -98,6 +135,12 @@ public class BayesInferenceHandler {
         }
     }
 
+    /**
+     * Sets the node name, and appends states.
+     *
+     * @param node
+     * @param disease
+     */
     private void initializeNode(ProbabilisticNode node, String disease) {
         node.setName(disease);
         node.appendState(STATE_POSITIVE);
