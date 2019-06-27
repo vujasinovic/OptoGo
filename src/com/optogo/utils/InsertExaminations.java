02b7875a0afa216f78a28e10a6b6d54961cba423
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-06-27 14:19:50 +0200

    InsertExamination fix

--- src/com/optogo/utils/InsertExaminations.java
+++ src/com/optogo/utils/InsertExaminations.java
@@ -13,6 +13,8 @@ import com.optogo.utils.enums.GenderType;
 import com.optogo.utils.enums.Race;
 import com.optogo.utils.enums.SymptomName;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,77 +27,32 @@ public class InsertExaminations {
         PatientRepository patientRepository = new PatientRepository();
         DiseaseRepository diseaseRepository = new DiseaseRepository();
         SymptomRepository symptomRepository = new SymptomRepository();
-        Examination examination1 = new Examination();
-        Patient patient1 = new Patient();
-        patient1.setFirstName("Dragan");
-        patient1.setLastName("Jovic");
-        patient1.setGender(GenderType.MALE);
-        patient1.setRace(Race.WHITE);
-        examination1.setPatient(patient1);
-        patientRepository.save(patient1);
 
-        List<Symptom> symptoms1 = new ArrayList<>();
-
-        Symptom symptom01 = new Symptom();
-        symptom01.setName(SymptomName.SYMPTOMS_OF_EYE);
-        symptoms1.add(symptom01);
-        symptomRepository.save(symptom01);
-        Symptom symptom02 = new Symptom();
-        symptom02.setName(SymptomName.ABNORMAL_APPEARING_SKIN);
-        symptoms1.add(symptom02);
-        symptomRepository.save(symptom02);
-        Symptom symptom03 = new Symptom();
-        symptom03.setName(SymptomName.CLOUDY_EYE);
-        symptoms1.add(symptom03);
-        symptomRepository.save(symptom03);
-        Symptom symptom04 = new Symptom();
-        symptom04.setName(SymptomName.EYE_BURNS_OR_STINGS);
-        symptoms1.add(symptom04);
-        symptomRepository.save(symptom04);
+        Symptom s1 = symptomRepository.findByName(SymptomName.ABNORMAL_MOVEMENT_OF_EYELID);
+        Symptom s2 = symptomRepository.findByName(SymptomName.PAIN_IN_EYE);
+        Symptom s3 = symptomRepository.findByName(SymptomName.BLINDNESS);
+        Symptom s4 = symptomRepository.findByName(SymptomName.ABNORMAL_BREATHING_SOUNDS);
+        List<Symptom> symptoms = new ArrayList<>();
+        symptoms.add(s1);
+        symptoms.add(s2);
+        symptoms.add(s3);
+        symptoms.add(s4);
 
 
-        examination1.setSymptoms(symptoms1);
+        Examination e1 = new Examination();
+        e1.setPatient(patientRepository.findById(1L));
+        e1.setSymptoms(symptoms);
+        e1.setDate(LocalDateTime.now());
+        e1.setDisease(diseaseRepository.findById(1L));
 
-        Disease disease1 = new Disease();
-        disease1.setName(DiseaseName.CATARACT);
-        diseaseRepository.save(disease1);
 
-        // EXAMINATION table not found
-        examinationRepository.save(examination1);
+        Examination e2 = new Examination();
+        e2.setPatient(patientRepository.findById(2L));
+        e2.setSymptoms(symptoms);
+        e2.setDate(LocalDateTime.now());
+        e2.setDisease(diseaseRepository.findById(2L));
 
-        Examination examination2 = new Examination();
-        examination1.setId((long)2);
-
-        Patient patient2 = new Patient();
-        patient2.setFirstName("Dragan");
-        patient2.setLastName("Draganovic");
-        patient2.setGender(GenderType.MALE);
-        patient2.setRace(Race.BLACK);
-        examination2.setPatient(patient2);
-        patientRepository.save(patient2);
-
-        List<Symptom> symptoms2 = new ArrayList<>();
-
-        Symptom symptom11 = new Symptom();
-        symptom11.setName(SymptomName.EYE_DEVIATION);
-        symptoms2.add(symptom11);
-        symptomRepository.save(symptom11);
-        Symptom symptom12 = new Symptom();
-        symptom12.setName(SymptomName.BACK_WEAKNESS);
-        symptoms2.add(symptom12);
-        symptomRepository.save(symptom12);
-        Symptom symptom13 = new Symptom();
-        symptom13.setName(SymptomName.CORYZA);
-        symptoms2.add(symptom13);
-        symptomRepository.save(symptom13);
-
-        examination2.setSymptoms(symptoms2);
-
-        Disease disease2 = new Disease();
-        disease2.setName(DiseaseName.AMBLYOPIA);
-        diseaseRepository.save(disease2);
-
-        examinationRepository.save(examination2);
+        examinationRepository.save(e1);
     }
 }
 
